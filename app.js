e8476fb826236d6aefc0d58e5ddf413dd1afdb1d
Gasparyan1999 <gasparyan.george1999@gmail.com>
2022-08-16 14:42:13 +0400

    New Fixes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import express from "express"
+import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import arr from "./data/Data.js"
+import arr from "./data/Data.js";
 
 const app = express();
 app.use(cors());
@@ -12,11 +12,9 @@ app.get("/", function (req, res) {
   res.send(arr);
 });
 
-
 app.get("/:id", function (req, res) {
-  const obj=arr[req.params["id"]]
-  res.send(obj)
+  const userViewObject = arr[req.params["id"]];
+  res.send(userViewObject);
 });
-
 
 app.listen(3000);

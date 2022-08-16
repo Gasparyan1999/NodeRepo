3496e25223bf1c87c481d698a6d83714c5a1f350
Gasparyan1999 <gasparyan.george1999@gmail.com>
2022-08-16 13:03:00 +0400

    New Fixed

--- app.js
+++ app.js
@@ -1,63 +1,22 @@
-const arr = [
-  {
-    id: "1",
-    name: "John",
-    create: "05-07-2012",
-    owner: "User1",
-  },
-  {
-    id: "2",
-    name: "Alis",
-    create: "15-02-2010",
-    owner: "User2",
-  },
-  {
-    id: "3",
-    name: "Max",
-    create: "14-08-2015",
-    owner: "User3",
-  },
-  {
-    id: "4",
-    name: "Messi",
-    create: "17-02-2002",
-    owner: "User4",
-  },
-  {
-    id: "5",
-    name: "Ronaldo",
-    create: "16-08-2015",
-    owner: "User5",
-  },
-  {
-    id: "6",
-    name: "Benzema",
-    create: "21-03-2004",
-    owner: "User6",
-  },
-  {
-    id: "7",
-    name: "Xavi",
-    create: "01-01-2000",
-    owner: "User7",
-  },
-];
+import express from "express"
+import bodyParser from "body-parser";
+import cors from "cors";
+import arr from "./data/Data.js"
 
-const bodyParser = require("body-parser");
-const express = require("express");
 const app = express();
-const cors = require("cors");
-
 app.use(cors());
+
 app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
   res.send(arr);
 });
 
-app.post("/", function (req, res,next) {
-  const obj=arr[req.body.id]
-   res.send(obj)
-})
+
+app.get("/:id", function (req, res) {
+  const obj=arr[req.params["id"]]
+  res.send(obj)
+});
+
 
 app.listen(3000);
---
+++ data/Data.js
@@ -0,0 +1,46 @@
+ const arr = [
+    {
+      id: "1",
+      name: "John",
+      create: "05-07-2012",
+      owner: "User1",
+    },
+    {
+      id: "2",
+      name: "Alis",
+      create: "15-02-2010",
+      owner: "User2",
+    },
+    {
+      id: "3",
+      name: "Max",
+      create: "14-08-2015",
+      owner: "User3",
+    },
+    {
+      id: "4",
+      name: "Messi",
+      create: "17-02-2002",
+      owner: "User4",
+    },
+    {
+      id: "5",
+      name: "Ronaldo",
+      create: "16-08-2015",
+      owner: "User5",
+    },
+    {
+      id: "6",
+      name: "Benzema",
+      create: "21-03-2004",
+      owner: "User6",
+    },
+    {
+      id: "7",
+      name: "Xavi",
+      create: "01-01-2000",
+      owner: "User7",
+    },
+  ];
+
+export default arr

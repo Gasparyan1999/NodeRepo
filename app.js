f50c950542fac961c8daad75b946dcaa4280e224
Gasparyan1999 <gasparyan.george1999@gmail.com>
2022-08-17 11:33:32 +0400

    New DataBase

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-import express from "express";
+import express, { query } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import arr from "./data/Data.js";
+import pool from "./Data/data.js";
 
+const PORT = process.env.port || 3000;
 const app = express();
 app.use(cors());
 
 app.use(bodyParser.json());
 
-app.get("/", function (req, res) {
-  res.send(arr);
+app.get("/", async function (req, res) {
+  const users = await pool.query("SELECT * FROM mytable");
+  res.send(users.rows);
 });
 
-app.get("/:id", function (req, res) {
-  const userViewObject = arr[req.params["id"]];
-  res.send(userViewObject);
+app.get("/:id", async function (req, res) {
+  const user = await pool.query("SELECT * FROM mytable");
+  res.send(user.rows[req.params["id"]]);
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`Server start in ${PORT}`);
+});

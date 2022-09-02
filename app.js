4a87eb953e17fbe218812184367ed1ed7dabb343
Gasparyan1999 <gasparyan.george1999@gmail.com>
2022-09-02 14:17:16 +0400

    Cards Api

--- app.js
+++ app.js
@@ -10,34 +10,53 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", async function (req, res) {
-  const users = await pool.query("SELECT * FROM mytable");
-  res.send(users.rows);
+  const cards = await pool.query("SELECT * FROM cards");
+  console.log(cards.rows);
+  res.send(cards.rows);
 });
 
-app.get("/:id", async function (req, res) {
-  const user = await pool.query(
-    `SELECT * FROM mytable WHERE id=${req.params["id"]}`
-  );
-  res.send(user.rows);
-});
+// app.get("/:id", async function (req, res) {
+//   const user = await pool.query(
+//     `SELECT * FROM mytable WHERE id=${req.params["id"]}`
+//   );
+//   res.send(user.rows);
+// });
+
+// app.post("/", async function (req, res) {
+//   console.log("Go")
+//   const user = await pool.query(
+//     `INSERT FROM mytable WHERE id=${req.params["id"]}`
+//   );
+//   res.send(user.rows);
+// });
 
 app.post("/", async function (req, res) {
-  const value = [
-    req.body[0][1],
-    req.body[1][1],
-    req.body[2][1],
-    req.body[3][1],
-    req.body[4],
-  ];
-  const user = await pool.query(
-    `INSERT INTO mytable VALUES
-    ('${value[4]}'::integer,'${value[0]}'::text, '${value[1]}'::text,'${value[2]}'::text, '${value[3]}'::text) RETURNING *`
-  );
-  res.json(user.rows);
+  // const value = [
+  //   req.body[0][1],
+  //   req.body[1][1],
+  //   req.body[2][1],
+  //   req.body[3][1],
+  //   req.body[4],
+  // ];
+  // const user = await pool.query(
+  //   `INSERT INTO cards VALUES
+  //   ('${value[4]}'::character,'${value[0]}'::text, '${value[1]}'::text,'${value[2]}'::text, '${value[3]}'::text) RETURNING *`
+  // );
+  // res.json(user.rows);
+  if (req.body[0].num) {
+    const value = [req.body[0].id, req.body[0].num];
+    const card = await pool.query(
+      `INSERT INTO cards VALUES
+      ('${value[0]}'::text,'${value[1]}'::integer) RETURNING *`
+    );
+    res.json(card.rows);
+  } else {
+    const value = [req.body[0]];
+    const card = await pool.query(`DELETE FROM cARDS WHERE id = '${value}'`);
+    res.json(card.rows);
+  }
 });
 
 app.listen(PORT, () => {
   console.log(`Server start in ${PORT}`);
 });
-
-
